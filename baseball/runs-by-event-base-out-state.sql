73fd836909a696d30acab4c384530516055ea96a
Nik Dudaev <nikolay.dudaev@icloud.com>
2024-07-04 18:02:17 +0200

    finalize events run value calculations

--- baseball/runs-by-event-base-out-state.sql
+++ baseball/runs-by-event-base-out-state.sql
@@ -7,273 +7,293 @@ with basis as (
 homeruns as (
   select season as "Season",
          "state" as "Base/Out State",
+		 event_cd,
          'Home Run' as "Event",
          count(*) as "N",
 		 sum(runs_roi) as "Runs to End of Inning",
 		 round(avg(runs_roi), 3) as "Average Runs"
   from basis
   where event_cd = '23'
-  group by season, "state"
+  group by season, "state", event_cd
 ),
 triples as (
   select season as "Season",
          "state" as "Base/Out State",
+		 event_cd,
          'Triple' as "Event",
          count(*) as "N",
 		 sum(runs_roi) as "Runs to End of Inning",
 		 round(avg(runs_roi), 3) as "Average Runs"
   from basis
   where event_cd = '22'
-  group by season, "state"
+  group by season, "state", event_cd
 ),
 doubles as (
   select season as "Season",
          "state" as "Base/Out State",
+		 event_cd,
          'Double' as "Event",
          count(*) as "N",
 		 sum(runs_roi) as "Runs to End of Inning",
 		 round(avg(runs_roi), 3) as "Average Runs"
   from basis
   where event_cd = '21'
-  group by season, "state"
+  group by season, "state", event_cd
 ),
 singles as (
   select season as "Season",
          "state" as "Base/Out State",
+		 event_cd,
          'Single' as "Event",
          count(*) as "N",
 		 sum(runs_roi) as "Runs to End of Inning",
 		 round(avg(runs_roi), 3) as "Average Runs"
   from basis
   where event_cd = '20'
-  group by season, "state"
+  group by season, "state", event_cd
 ),
 errors as (
   select season as "Season",
          "state" as "Base/Out State",
+		 event_cd,
          'Error' as "Event",
          count(*) as "N",
 		 sum(runs_roi) as "Runs to End of Inning",
 		 round(avg(runs_roi), 3) as "Average Runs"
   from basis
   where event_cd = '18'
-  group by season, "state"
+  group by season, "state", event_cd
 ),
 interference as (
   select season as "Season",
          "state" as "Base/Out State",
+		 event_cd,
          'Interference' as "Event",
          count(*) as "N",
 		 sum(runs_roi) as "Runs to End of Inning",
 		 round(avg(runs_roi), 3) as "Average Runs"
   from basis
   where event_cd = '17'
-  group by season, "state"
+  group by season, "state", event_cd
 ),
 passed_ball as (
   select season as "Season",
          "state" as "Base/Out State",
+		 event_cd,
          'Passed Ball' as "Event",
          count(*) as "N",
 		 sum(runs_roi) as "Runs to End of Inning",
 		 round(avg(runs_roi), 3) as "Average Runs"
   from basis
   where event_cd = '10'
-  group by season, "state"
+  group by season, "state", event_cd
 ),
 sac_bunt as (
   select season as "Season",
          "state" as "Base/Out State",
+		 event_cd,
          'Sac Bunt' as "Event",
          count(*) as "N",
 		 sum(runs_roi) as "Runs to End of Inning",
 		 round(avg(runs_roi), 3) as "Average Runs"
   from basis
   where bunt_fl = 'T' and outs_ct < 2 and not bases in ('000')
-  group by season, "state"
+  group by season, "state", event_cd
 ),
 bunt as (
   select season as "Season",
          "state" as "Base/Out State",
+		 event_cd,
          'Bunt' as "Event",
          count(*) as "N",
 		 sum(runs_roi) as "Runs to End of Inning",
 		 round(avg(runs_roi), 3) as "Average Runs"
   from basis
   where bunt_fl = 'T' and not (outs_ct < 2 and not bases in ('000'))
-  group by season, "state"
+  group by season, "state", event_cd
 ),
 wild_pitch as (
   select season as "Season",
          "state" as "Base/Out State",
+		 event_cd,
          'Wild Pitch' as "Event",
          count(*) as "N",
 		 sum(runs_roi) as "Runs to End of Inning",
 		 round(avg(runs_roi), 3) as "Average Runs"
   from basis
   where event_cd = '9'
-  group by season, "state"
+  group by season, "state", event_cd
 ),
 balk as (
   select season as "Season",
          "state" as "Base/Out State",
+		 event_cd,
          'Balk' as "Event",
          count(*) as "N",
 		 sum(runs_roi) as "Runs to End of Inning",
 		 round(avg(runs_roi), 3) as "Average Runs"
   from basis
   where event_cd = '11'
-  group by season, "state"
+  group by season, "state", event_cd
 ),
 bb as (
   select season as "Season",
          "state" as "Base/Out State",
+		 event_cd,
          'Non-intentional Walk' as "Event",
          count(*) as "N",
 		 sum(runs_roi) as "Runs to End of Inning",
 		 round(avg(runs_roi), 3) as "Average Runs"
   from basis
   where event_cd = '14'
-  group by season, "state"
+  group by season, "state", event_cd
 ),
 ibb as (
   select season as "Season",
          "state" as "Base/Out State",
+		 event_cd,
          'Intentional Walk' as "Event",
          count(*) as "N",
 		 sum(runs_roi) as "Runs to End of Inning",
 		 round(avg(runs_roi), 3) as "Average Runs"
   from basis
   where event_cd = '15'
-  group by season, "state"
+  group by season, "state", event_cd
 ),
 stolen_base as (
   select season as "Season",
          "state" as "Base/Out State",
+		 event_cd,
          'Stolen Base' as "Event",
          count(*) as "N",
 		 sum(runs_roi) as "Runs to End of Inning",
 		 round(avg(runs_roi), 3) as "Average Runs"
   from basis
   where event_cd = '4'
-  group by season, "state"
+  group by season, "state", event_cd
 ),
 def_indifference as (
   select season as "Season",
          "state" as "Base/Out State",
+		 event_cd,
          'Defensive Indifference' as "Event",
          count(*) as "N",
 		 sum(runs_roi) as "Runs to End of Inning",
 		 round(avg(runs_roi), 3) as "Average Runs"
   from basis
   where event_cd = '5'
-  group by season, "state"
+  group by season, "state", event_cd
 ),
 pickoff as (
   select season as "Season",
          "state" as "Base/Out State",
+		 event_cd,
          'Pickoff' as "Event",
          count(*) as "N",
 		 sum(runs_roi) as "Runs to End of Inning",
 		 round(avg(runs_roi), 3) as "Average Runs"
   from basis
   where event_cd = '8'
-  group by season, "state"
+  group by season, "state", event_cd
 ),
 gen_out as (
   select season as "Season",
          "state" as "Base/Out State",
+		 event_cd,
          'Out' as "Event",
          count(*) as "N",
 		 sum(runs_roi) as "Runs to End of Inning",
 		 round(avg(runs_roi), 3) as "Average Runs"
   from basis
   where event_cd = '2'
-  group by season, "state"
+  group by season, "state", event_cd
 ),
 strikeout as (
   select season as "Season",
          "state" as "Base/Out State",
+		 event_cd,
          'Strikeout' as "Event",
          count(*) as "N",
 		 sum(runs_roi) as "Runs to End of Inning",
 		 round(avg(runs_roi), 3) as "Average Runs"
   from basis
   where event_cd = '3'
-  group by season, "state"
+  group by season, "state", event_cd
 ),
 caught_stealing as (
   select season as "Season",
          "state" as "Base/Out State",
+		 event_cd,
          'Caught Stealing' as "Event",
          count(*) as "N",
 		 sum(runs_roi) as "Runs to End of Inning",
 		 round(avg(runs_roi), 3) as "Average Runs"
   from basis
   where event_cd = '6'
-  group by season, "state"
+  group by season, "state", event_cd
 ),
 events as (
-  select "Season", "Base/Out State", "Event", "N", "Runs to End of Inning", "Average Runs"
+  select "Season", "Base/Out State", event_cd, "Event", "N", "Runs to End of Inning", "Average Runs"
   from homeruns
   union all
-  select "Season", "Base/Out State", "Event", "N", "Runs to End of Inning", "Average Runs"
+  select "Season", "Base/Out State", event_cd, "Event", "N", "Runs to End of Inning", "Average Runs"
   from triples
   union all
-  select "Season", "Base/Out State", "Event", "N", "Runs to End of Inning", "Average Runs"
+  select "Season", "Base/Out State", event_cd, "Event", "N", "Runs to End of Inning", "Average Runs"
   from doubles
   union all
-  select "Season", "Base/Out State", "Event", "N", "Runs to End of Inning", "Average Runs"
+  select "Season", "Base/Out State", event_cd, "Event", "N", "Runs to End of Inning", "Average Runs"
   from singles
   union all
-  select "Season", "Base/Out State", "Event", "N", "Runs to End of Inning", "Average Runs"
+  select "Season", "Base/Out State", event_cd, "Event", "N", "Runs to End of Inning", "Average Runs"
   from errors
   union all
-  select "Season", "Base/Out State", "Event", "N", "Runs to End of Inning", "Average Runs"
+  select "Season", "Base/Out State", event_cd, "Event", "N", "Runs to End of Inning", "Average Runs"
   from interference
   union all
-  select "Season", "Base/Out State", "Event", "N", "Runs to End of Inning", "Average Runs"
+  select "Season", "Base/Out State", event_cd, "Event", "N", "Runs to End of Inning", "Average Runs"
   from passed_ball
   union all
-  select "Season", "Base/Out State", "Event", "N", "Runs to End of Inning", "Average Runs"
+  select "Season", "Base/Out State", event_cd, "Event", "N", "Runs to End of Inning", "Average Runs"
   from sac_bunt
   union all
-  select "Season", "Base/Out State", "Event", "N", "Runs to End of Inning", "Average Runs"
+  select "Season", "Base/Out State", event_cd, "Event", "N", "Runs to End of Inning", "Average Runs"
   from bunt
   union all
-  select "Season", "Base/Out State", "Event", "N", "Runs to End of Inning", "Average Runs"
+  select "Season", "Base/Out State", event_cd, "Event", "N", "Runs to End of Inning", "Average Runs"
   from wild_pitch
   union all
-  select "Season", "Base/Out State", "Event", "N", "Runs to End of Inning", "Average Runs"
+  select "Season", "Base/Out State", event_cd, "Event", "N", "Runs to End of Inning", "Average Runs"
   from balk
   union all
-  select "Season", "Base/Out State", "Event", "N", "Runs to End of Inning", "Average Runs"
+  select "Season", "Base/Out State", event_cd, "Event", "N", "Runs to End of Inning", "Average Runs"
   from bb
   union all
-  select "Season", "Base/Out State", "Event", "N", "Runs to End of Inning", "Average Runs"
+  select "Season", "Base/Out State", event_cd, "Event", "N", "Runs to End of Inning", "Average Runs"
   from ibb
   union all
-  select "Season", "Base/Out State", "Event", "N", "Runs to End of Inning", "Average Runs"
+  select "Season", "Base/Out State", event_cd, "Event", "N", "Runs to End of Inning", "Average Runs"
   from stolen_base
   union all
-  select "Season", "Base/Out State", "Event", "N", "Runs to End of Inning", "Average Runs"
+  select "Season", "Base/Out State", event_cd, "Event", "N", "Runs to End of Inning", "Average Runs"
   from def_indifference
   union all
-  select "Season", "Base/Out State", "Event", "N", "Runs to End of Inning", "Average Runs"
+  select "Season", "Base/Out State", event_cd, "Event", "N", "Runs to End of Inning", "Average Runs"
   from pickoff
   union all
-  select "Season", "Base/Out State", "Event", "N", "Runs to End of Inning", "Average Runs"
+  select "Season", "Base/Out State", event_cd, "Event", "N", "Runs to End of Inning", "Average Runs"
   from gen_out
   union all
-  select "Season", "Base/Out State", "Event", "N", "Runs to End of Inning", "Average Runs"
+  select "Season", "Base/Out State", event_cd, "Event", "N", "Runs to End of Inning", "Average Runs"
   from strikeout
   union all
-  select "Season", "Base/Out State", "Event", "N", "Runs to End of Inning", "Average Runs"
+  select "Season", "Base/Out State", event_cd, "Event", "N", "Runs to End of Inning", "Average Runs"
   from caught_stealing
 ),
 cleaned as (
   select "Season",
        "Base/Out State" as "Numeric B/O State",
+	    event_cd,
        case
 	    when "Base/Out State" = '000 0' THEN 1
 	    when "Base/Out State" = '100 0' THEN 4
@@ -335,17 +355,39 @@ cleaned as (
 re as (
   select season,
          "state",
-         avg(rv_start) as rv_start
+		 event_cd,
+		 avg(runs_scored) as runs_scored,
+         avg(rv_start) as rv_start,
+		 avg(rv_end) as rv_end,
+		 avg(run_value) as run_value
   from run_expectancy.full_rv_1912_2023
-  group by season, "state"
+  group by season, "state", event_cd
 ),
 cleaned_with_re as (
   select cl.*,
-         r.rv_start as "Starting RE",
-		 cl."Average Runs" - r.rv_start as "Run Value"
+		 "Average Runs" - r.rv_start as "Original RE (Avg Runs - Starting RE)",
+		 r.runs_scored as "Runs Scored",
+         r.rv_start as "Starting RE (overall B/O State)",
+		 r.rv_end as "Ending RE (overall B/O State",
+		 r.rv_start + r.runs_scored as "Ending RE (Event)",
+		 r.run_value as "Run Value"
   from cleaned cl
-  left join re r on cl."Season" = r.season and cl."Numeric B/O State" = r."state"
+  left join re r on cl."Season" = r.season and cl."Numeric B/O State" = r."state" and cl.event_cd = r.event_cd
 )
-select *
+select bases_state_order,
+       event_cd,
+       "Season",
+       "Numeric B/O State",
+	   "Base/Out State",
+	   "Event", 
+	   "N", 
+	   "Runs to End of Inning", 
+	   "Average Runs",
+	   "Original RE (Avg Runs - Starting RE)",
+	   "Runs Scored",
+       "Starting RE (overall B/O State)",
+	   "Ending RE (overall B/O State",
+	   "Ending RE (Event)",
+	   "Run Value"
 from cleaned_with_re
 order by "Season", bases_state_order;
